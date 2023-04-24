b88d0b9236a5e16b6db59a455ccf304b4c608fb0
Mohammad <mohammadimaan484@gmail.com>
2023-04-24 04:12:15 -0400

    Completed connect chatbot page, without connection to actual chatbot

---
+++ app/controllers/connect_controller.rb
@@ -0,0 +1,27 @@
+class ConnectController < ApplicationController
+  
+  def chatbot
+    session[:message_history] ||= ["Hello, I'm WishConnect, an AI assistant to help you on your wish journey. How can I be of assistance today?"]
+  end
+
+  def call_chat_api
+    prompt = params[:prompt]
+    session[:message_history] ||= [] # Initialize message history if it doesn't exist
+    session[:message_history] << prompt
+    session[:message_history] << "response"
+
+    redirect_to "/connect/chatbot"
+  end
+
+  def reset_message_log
+    reset_session
+
+    redirect_to "/connect/chatbot"
+  end
+
+  private 
+
+  def session_params
+    params.permit(:messages, :prompt)
+  end
+end
